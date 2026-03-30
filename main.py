ca2070e6ae0619d5e35cf9cdd1e8acb3dcdfa4b7
KaoryPrime <kamil.cinar@etu.univ-smb.fr>
2026-03-30 08:15:16 +0200

    Implement main menu for maths functions

---
+++ main.py
@@ -0,0 +1,29 @@
+def menu():
+    print("=== MATHS FI ===")
+    print("1. Interets Simples")
+    print("2. Interets Composes")
+    print("3. Annuites")
+    print("4. Emprunts Indivis")
+    print("5. Utilitaire dates")
+    print("0. Quitter")
+    return int(input("Choix: "))
+
+while True:
+    c = menu()
+    if c == 0: 
+        break
+    elif c == 1: 
+        from f12 import fiche1
+        fiche1()
+    elif c == 2: 
+        from f12 import fiche2
+        fiche2()
+    elif c == 3: 
+        from f34 import fiche3
+        fiche3()
+    elif c == 4: 
+        from f34 import fiche4
+        fiche4()
+    elif c == 5: 
+        from outils import util_dates
+        util_dates()
